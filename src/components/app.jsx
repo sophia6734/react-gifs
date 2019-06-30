4722a98cb6e56fc563d137d39d8270955710e892
sophia wu <sophia6734wu@gmail.com>
2019-06-30 16:23:10 +0800

    giphy react

---
+++ src/components/app.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+  }
+
+  search = (query) => {
+    giphy('6Oq48K4n486YeiwRC2uKRONy7WDP86sa').search({
+      q: query,
+      rating: 'g'
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data,
+        limit: 10
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/gif.jsx
@@ -0,0 +1,20 @@
+/* eslint jsx-a11y/alt-text: off */
+/* eslint jsx-a11y/no-noninteractive-element-interactions: off */
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  render () {
+    const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`;
+    return (
+      <img src={src} alt="" className="gif" onClick={this.handleClick} />
+    );
+  }
+}
+
+export default Gif;
